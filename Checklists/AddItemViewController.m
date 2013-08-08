5df10d7cc7585472bad62e10774e8af6df080ef6
Mike Perry <Mike@Mikes-MacBook-Pro.local>
2013-08-08 00:51:36 -0400

    Started chapter 4, working on adding the AddItemViewController. Had to stop at “Adding New ChecklistItems”

---
+++ Checklists/AddItemViewController.m
@@ -0,0 +1,81 @@
+//
+//  AddItemViewController.m
+//  Checklists
+//
+//  Created by Mike Perry on 8/8/13.
+//  Copyright (c) 2013 Mike Perry. All rights reserved.
+//
+
+#import "AddItemViewController.h"
+
+@interface AddItemViewController ()
+
+@end
+
+@implementation AddItemViewController
+
+- (id)initWithStyle:(UITableViewStyle)style
+{
+    self = [super initWithStyle:style];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+
+    // Uncomment the following line to preserve selection between presentations.
+    // self.clearsSelectionOnViewWillAppear = NO;
+ 
+    // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
+    // self.navigationItem.rightBarButtonItem = self.editButtonItem;
+}
+
+- (void)viewWillAppear:(BOOL)animated
+{
+    [super viewWillAppear:animated];
+    
+    [self.textField becomeFirstResponder];
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+- (IBAction)cancel
+{
+    [self.presentingViewController dismissViewControllerAnimated:YES
+                                                      completion:nil];
+    
+}
+
+- (IBAction)done
+{
+    NSLog(@"Contents of the text field: %@", self.textField.text);
+    [self.presentingViewController dismissViewControllerAnimated:YES completion:nil];
+}
+
+- (NSIndexPath *)tableView:(UITableView *)tableView willSelectRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    return nil;
+}
+
+#pragma mark - UITextField Delegate
+
+- (BOOL)textField:(UITextField *)theTextField shouldChangeCharactersInRange:(NSRange)range replacementString:(NSString *)string
+{
+    NSString *newText = [theTextField.text stringByReplacingCharactersInRange:range withString:string];
+    
+    self.doneBarButton.enabled = ([newText length] > 0);
+    
+    return YES;
+    
+}
+
+
+@end
